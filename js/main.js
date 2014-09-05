8a06eda082be2760540fb82dbf63285176aa10b0
Bruno Muller <brunohmuller@gmail.com>
2014-09-05 14:18:00 -0300

    backdrop independent

--- js/main.js
+++ js/main.js
@@ -1,37 +1,54 @@
 $(function(){
+	/**
+	Default html class selectors:
+
+	**/
+
+
 	'use strict';
 	var $lauchBtn =  $('.js-lauch-modal'),
-		$closeBtn =  $('.js-modal-close'),
 		$modal = $('.js-modal'),
-		$modalBackdrop = $('.js-modal-backdrop');
-
-
+		$modalBackdrop = $('<div class="backdrop"></div>');
+		
 	//open modal
-	var openModal = function() {
-		$modal.removeClass('hidden').addClass('is-open');
+	var openModal = function($modalElement) {
+		var $closeBtn = $modalElement.find('.js-modal-close');	
+		
+		$('body').append($modalBackdrop);
+
+		//window.getComputedStyle(element).cssText;
+		$modalBackdrop.addClass('is-open');
+
+		$modalElement.removeClass('hidden').addClass('is-open');
 		$('body').addClass('no-scroll');
 
-		$modalBackdrop.on('click', function(){
+		$modalBackdrop.on('click', function() {
+			closeModal();
+		});
+
+		$closeBtn.on('click', function() {
+			console.log('cl');
 			closeModal();
 		});
 	};
 
 	var closeModal = function() {
 		$modal.removeClass('is-open');
+		$modalBackdrop.removeClass('is-open');
 		setTimeout(afterClose, 500);
 	};
 
 	var afterClose = function () {
 		$modal.addClass('hidden');
 		$('body').removeClass('no-scroll');
+		$modalBackdrop.remove();
 	};
 
 	$lauchBtn.click(function(){
-		openModal();
+		openModal($modal);
 	});
-	$closeBtn.click(function(){
-		console.log('cl');
-		closeModal();
-	});
+
+
+	
 	
 });
